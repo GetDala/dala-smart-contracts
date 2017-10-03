079b09c1e3bf529b23c148d28b14705037236c40
Ross McEwan <rossmcewan@gmail.com>
2017-10-03 12:36:22 +0200

    removed metacoin.js. changed auth amount to zero on multisig deploy.

--- migrations/2_deploy_multisigwallet.js
+++ migrations/2_deploy_multisigwallet.js
@@ -2,7 +2,7 @@ var MultiSigWalletWithDailyLimit = artifacts.require('./MultiSigWalletWithDailyL
 
 module.exports = function (deployer, network) {
   if (network == 'development') {
-    deployer.deploy(MultiSigWalletWithDailyLimit, ['0xc40a866df87e14fe9ea65d49766b8b27bef5739b', '0x7997d47a3c6480b8b6dd7650c45b997e67169b56'], 2, 100 * Math.pow(10, 18));
+    deployer.deploy(MultiSigWalletWithDailyLimit, ['0xc40a866df87e14fe9ea65d49766b8b27bef5739b', '0x7997d47a3c6480b8b6dd7650c45b997e67169b56'], 2, 0);
   }
   if (network == 'ropsten') {
     //account1
@@ -14,6 +14,6 @@ module.exports = function (deployer, network) {
     //password = p@ssw0rd123
     //private key = d6741ef2d9807f0ad674c09976b59d12381323b5e7c5af0a4afda8a3281e1dfb
     //address = 0xDEE8ae658dcfbd41E0a03150D4370B3921F96870
-    deployer.deploy(MultiSigWalletWithDailyLimit, ['0x4F0A6c734AE57998b02aA9f32674Fa5A37988De6', '0xDEE8ae658dcfbd41E0a03150D4370B3921F96870'], 2, 100 * Math.pow(10, 18));
+    deployer.deploy(MultiSigWalletWithDailyLimit, ['0x4F0A6c734AE57998b02aA9f32674Fa5A37988De6', '0xDEE8ae658dcfbd41E0a03150D4370B3921F96870'], 2, 0);
   }
 };
